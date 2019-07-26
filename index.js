dc1132efecce29bcacf08b0b7db5faaa5a56ada3
Gustavo Fenilli <gustavofenilli@hotmail.com>
2019-07-26 15:20:06 -0300

    removed test index

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var path_1 = __importDefault(require("path"));
-var Bootstraper_1 = __importDefault(require("./Bootstraper"));
-new Bootstraper_1.default(path_1.default.resolve(__dirname));
